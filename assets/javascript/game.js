8d37592d6ab8eaa01e94ff61a85d73aee663335d
ghostfountain <30249360+LandrumTrev@users.noreply.github.com>
2018-08-14 16:29:41 -0400

    finished except userKey

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,17 +9,44 @@ let compChoice = "x";
 let wins = 0;
 let losses = 0;
 
+let guessesLeft = 9;
+let yourGuesses = [];
+
 let reset = function () {
     guessesLeft = 9;
     document.getElementById("uLeft").textContent = guessesLeft;
     yourGuesses = [];
     document.getElementById("uGuess").textContent = yourGuesses;
-    compChoice = letterBin[Math.floor(Math.random() * letterBin.length)];
+    let compChoice = "x";
+    // compChoice = letterBin[Math.floor(Math.random() * letterBin.length)];
     document.getElementById("cLetter").textContent = compChoice;
 }
 
-let guessesLeft = 9;
-let yourGuesses = [" a", " b", " c"];
+let play = function () {
+
+    if (userChoice === compChoice) {
+        reset();
+        wins = wins + 1;
+        document.getElementById("uWin").textContent = wins;
+    } else if (userChoice !== compChoice && guessesLeft > 1) {
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("uLeft").textContent = guessesLeft;
+        yourGuesses.push(" " + userChoice);
+        document.getElementById("uGuess").textContent = yourGuesses;
+    } else {
+        reset();
+        losses = losses + 1;
+        document.getElementById("uLose").textContent = losses;
+    };
+}
+
+
+
+
+
+
+
+
 
 
 let userChoice = "x";
@@ -29,12 +56,6 @@ document.onkeyup = function (event) {
     // THIS STATEMENT DOES NOT WORK
     // WHY WONT THE VALUE OF THE GLOBAL userChoice VARIABLE CHANGE?
     userChoice = event.key.toLowerCase();
-
-    if (userChoice === compChoice) {
-        wins++;
-        reset();
-    }
-
 
 }
 
